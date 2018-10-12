c763869c22e4bc817b62e91d5ace6fd8927bb675
Brent Ertz <brent.ertz@ca.com>
2018-10-12 09:15:07 -0600

    WIP: Convert Truncate

--- src/library/Truncate/Truncate.js
+++ src/library/Truncate/Truncate.js
@@ -1,64 +1,19 @@
 /* @flow */
 import React, { PureComponent } from 'react';
 import debounce from 'lodash.debounce';
-import { ellipsis } from 'polished';
-import { createStyledComponent } from '../styles';
-import _Tooltip from '../Tooltip';
+import { Truncate as Root, Tooltip } from './styled';
 
-type Props = {
-  /** Content of Truncate */
-  children?: React$Node
-};
+import type { TruncateProps, TruncateState } from './types';
 
-type State = {
-  showTooltip: boolean
-};
-
-const styles = {
-  root: ({ theme: baseTheme }) => ({
-    pointerEvents: 'all', // Necessary because of Button Inner's pointerEvents: none
-    ...ellipsis('100%'),
-
-    '&:focus': {
-      outline: `1px solid ${baseTheme.color_theme}`,
-      outlineOffset: -1
-    }
-  }),
-  tooltip: {
-    // This style is only necessary because we're using Truncate as a Button child
-    whiteSpace: 'normal'
-  }
-};
-
-const Root = createStyledComponent('span', styles.root, {
-  displayName: 'Truncate'
-});
-const Tooltip = createStyledComponent(_Tooltip, styles.tooltip);
-
-export default class Truncate extends PureComponent<Props, State> {
+export default class Truncate extends PureComponent<
+  TruncateProps,
+  TruncateState
+> {
   state = {
     showTooltip: false
   };
 
   root: ?HTMLElement;
-
-  setRootRef = (node: HTMLElement) => {
-    this.root = node;
-  };
-
-  toggleTooltip = () => {
-    const rootNode = this.root;
-
-    if (rootNode) {
-      const offsetWidth = rootNode.offsetWidth + 1; // `+ 1` necessary for Edge & IE11
-      const showTooltip = Boolean(rootNode.scrollWidth > offsetWidth);
-      if (this.state.showTooltip !== showTooltip) {
-        this.setState({
-          showTooltip
-        });
-      }
-    }
-  };
 
   componentDidMount() {
     this.toggleTooltip();
@@ -67,7 +22,6 @@ export default class Truncate extends PureComponent<Props, State> {
   componentDidUpdate() {
     this.toggleTooltip();
   }
-  handleWindowResize = debounce(this.toggleTooltip, 100);
 
   render() {
     const { children, ...restProps } = this.props;
@@ -83,4 +37,24 @@ export default class Truncate extends PureComponent<Props, State> {
       content
     );
   }
+
+  setRootRef = (node: ?HTMLElement) => {
+    this.root = node;
+  };
+
+  handleWindowResize = debounce(this.toggleTooltip, 100);
+
+  toggleTooltip = () => {
+    const rootNode = this.root;
+
+    if (rootNode) {
+      const offsetWidth = rootNode.offsetWidth + 1; // `+ 1` necessary for Edge & IE11
+      const showTooltip = Boolean(rootNode.scrollWidth > offsetWidth);
+      if (this.state.showTooltip !== showTooltip) {
+        this.setState({
+          showTooltip
+        });
+      }
+    }
+  };
 }
